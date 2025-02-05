8c5b34c2b356e013d03e530771f6bad00523ff8a
ML-RefVm-358739\usuario.qaracter <jesus.hernandez@imagina-bpo.es>
2025-02-05 12:37:45 +0000

    first commit

---
+++ src/main/java/com/example/Loan/Approval/System/LoanApprovalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Loan.Approval.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApprovalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApprovalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Loan/Approval/System/LoanApprovalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loan.Approval.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApprovalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
